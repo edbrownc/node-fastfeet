0b6cc4d7a9305d79f933e1c200bd088954fcac7e
Ed <ed@pop-os.localdomain>
2020-04-08 19:06:11 -0700

    Delivery status added to orders

--- src/app/controllers/DeliveredOrdersController.js
+++ src/app/controllers/DeliveredOrdersController.js
@@ -17,7 +17,14 @@ class DeliveredOrdersController {
         },
       },
       order: ['start_date'],
-      attributes: ['id', 'product', 'start_date', 'end_date', 'created_at'],
+      attributes: [
+        'id',
+        'product',
+        'start_date',
+        'end_date',
+        'created_at',
+        'status',
+      ],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
